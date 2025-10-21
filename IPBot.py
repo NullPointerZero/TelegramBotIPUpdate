3eac0009b333bddfbb1fe9ea9d9e115c5a9594b8
NullPointerZero <Mar-Hoff@mailbox.org>
2025-10-21 20:27:20 +0200

    [CHORE] (reusable) - marked some functions that are potentially reusable.

--- IPBot.py
+++ IPBot.py
@@ -6,6 +6,9 @@ import json
 from config_loader import load_config
 from IP_comparator import get_public_ip
 
+
+# Potentially reusable for other Projects. Or to make the Bot more interactive.
+# No interaction planned yet.
 def send(msg: str, token: str, chat_id: str) -> None:
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     r = requests.post(url, data={"chat_id": chat_id, "text": msg, "disable_web_page_preview": True}, timeout=10)
@@ -14,6 +17,9 @@ def send(msg: str, token: str, chat_id: str) -> None:
     if not ok:
         raise RuntimeError(f"Telegram-API-Fehler: {r.text}")
 
+
+# This function is not reuasable anyway. it only checks wether the IP changed. It uses the config for
+# it.
 def did_IP_change(filename = "config.json"):
     with open(filename, "r", encoding="utf-8") as f:
         data = json.load(f)
@@ -26,6 +32,8 @@ def did_IP_change(filename = "config.json"):
         write_IP_to_File(new_IP_address)
         print("[did IP change] new IP Address written")
 
+# This function writes the IP to the config file. It uses a certain Identifier for it.
+# potentially it could be reused in another project. Maybe i should make it more reusable.
 def write_IP_to_File(new_ip, filename = "config.json"):
     with open(filename, "r", encoding="utf-8") as f:
         data = json.load(f)
