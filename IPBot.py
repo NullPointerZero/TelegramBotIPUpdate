97c1b03d4d737f355262ae8b604387e78d23d695
NullPointerZero <Mar-Hoff@mailbox.org>
2025-10-21 20:15:51 +0200

    [FEAT] (write IP) - made a feature to write the IP Address to a file. WHY: Because otherwise it will send the IP everytime the Programm is called. So as i plan it: daily...

--- IPBot.py
+++ IPBot.py
@@ -23,11 +23,24 @@ def did_IP_change(filename = "config.json"):
     if new_IP_address != old_IP_address:
         token, chat_id = load_config()
         send(new_IP_address, token, chat_id)
+        write_IP_to_File(new_IP_address)
+        print("[did IP change] new IP Address written")
+
+def write_IP_to_File(new_ip, filename = "config.json"):
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    if "old_IP_Address" not in data:
+        raise KeyError("identifier 'old_IP_Address' not Found")
+
+    data["old_IP_Address"] = new_ip
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
 
     did_IP_change()
     token, chat_id = load_config()
-    send("Long live the King", token, chat_id)
+    #send("Long live the King", token, chat_id)
     print("Gesendet.")
